66745b3cbf57f95c7d3f6d0fdf3c3b89e7e41b9e
Melvin Lim <lim.dongxing@gmail.com>
2016-08-10 17:19:43 -0700

    strange pattern

--- graphics.js
+++ graphics.js
@@ -9,31 +9,59 @@ var LIME="#00FF00"
 var canvas=document.getElementById("myCanvas");
 var cv=canvas.getContext("2d");
 var img=cv.createImageData(NCOLS,NROWS);
-//alert("asdf");
+//var img=cv.createImageData(NROWS,NROWS);
 setInterval(update,10);
 
 var i;
 var j;
 var t=0;
 
+/*
+var Y0= -1.2;
+var Y1= +1.2;
+var YS=  0.01;
+
+var X0= -1.6;
+var X1= +1.6;
+var XS=  0.01;
+*/
+
+var Y0= -NROWS/100.0;
+var Y1= +NROWS/100.0;
+var YS=  0.05;
+
+var X0= -NCOLS/100.0;
+var X1= +NCOLS/100.0;
+var XS=  0.05;
+
+var x;
+var y=Y0;
+
 function update(){
-	t+=1;
-	for(i=0;i<NROWS*NCOLS*4;i+=NCOLS){
+	t+=0.01;
+	y=Y0;
+	for(i=0;i<NROWS*NCOLS*4*4;i+=NCOLS*4){
+		y+=YS;
+		x=X0;
 		for(j=0;j<NCOLS*4;j+=4){
-			img.data[i+j+0]=0;
-			img.data[i+j+1]=(255+t)%255;
+			x+=XS;
+//			img.data[i+j+1]=(255+t)%255;
+//			img.data[i+j+1]=255*Math.sin(t);
+//			if((Math.sin(x)+Math.cos(y))>0){
+			img.data[i+j+0]=255*Math.sin(x-y+t);
+			img.data[i+j+1]=255*Math.sin(x+y+t);
+/*
+			if((Math.sin(x))>0){
+				img.data[i+j+0]=0;
+				img.data[i+j+1]=255*Math.sin(t+x);
+			}else{
+				img.data[i+j+0]=255*Math.sin(t+x);
+				img.data[i+j+1]=0;
+			}
+*/
 			img.data[i+j+2]=0;
 			img.data[i+j+3]=255;
 		}
 	}
 	cv.putImageData(img,0,0);
 }
-/*
-	setColor(RED);
-	cv.fillRect(0,0,NCOLS,(NROWS));
-	setColor(BLUE);
-	cv.fillRect(0,0,NCOLS,(NROWS));
-function setColor(color){
-	cv.fillStyle=color;
-}
-*/
